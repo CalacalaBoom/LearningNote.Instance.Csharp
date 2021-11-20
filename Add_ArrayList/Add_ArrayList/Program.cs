c8ce25d2454124fd0606f78aa9fef36972ab8554
CalacalaBoom <zhangyu19990502@163.com>
2021-11-20 15:30:42 +0800

    ArrayList的添加与删除方法

---
+++ Add_ArrayList/Add_ArrayList/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;//ArrayList类位于System.Collections,需要using才能使用
+
+namespace Add_ArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("向ArrayList集合中添加元素有Add()和Insert()两种方法：");
+            Console.WriteLine("\n原数组：1 2 3 4 5 6 7 8 9 0");
+            Console.WriteLine("1.Add()  2.Insert()  0.Exit");
+            int[] arr = new int[] {1,2,3,4,5,6,7,8,9,0};
+            ArrayList List = new ArrayList(arr);
+            bool isExit = true;
+            do
+            {
+                string i = Console.ReadLine();
+                switch (i)
+                {
+                    case "1":
+                        AddtoArrayList(List);
+                        break;
+                    case "2":
+                        InserttoArrayList(List);
+                        break;
+                    case "0":
+                        isExit = false;
+                        break;
+                }
+            } while (isExit);
+        }
+        static void AddtoArrayList(ArrayList List)
+        {
+            Console.WriteLine("Add()方法向ArrayList对象的末尾添加元素");
+            for(int i = 0;i < 5;i++)
+            {
+                List.Add(i);
+            }
+            Console.Write("插入后的数组为：");
+            foreach (int n in List)
+            {
+                Console.Write(n+" ");
+            }
+            Console.WriteLine();
+        }
+        static void InserttoArrayList(ArrayList List)
+        {
+            Console.WriteLine("Insert()方法向ArrayList对象的指定位置插入元素");
+            List.Insert(6,6);
+            Console.Write("插入后的数组为：");
+            foreach (int n in List)
+            {
+                Console.Write(n + " ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ Delete_ArrayList/Delete_ArrayList/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Delete_ArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ArrayList类提供了以下四种方法来删除元素：");
+            Console.WriteLine("1.Clear()  2.Remove()  3.RemoveAAt()  4.RemoveRange()  0.Exit");
+            bool isExit = true;
+            do
+            {
+                string i = Console.ReadLine();
+                switch (i)
+                {
+                    case "1":
+                        ClearArrayList();
+                        break;
+                    case "2":
+                        RemoveArrayList();
+                        break;
+                    case "3":
+                        RemoveAtArrayList();
+                        break;
+                    case "4":
+                        RemoveRangeArrayList();
+                        break;
+                    case "0":
+                        isExit = false;
+                        break;
+                }
+            } while (isExit);
+        }
+        static void ClearArrayList()
+        {
+            ArrayList list = new ArrayList();
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(i);
+            }
+            Console.Write("原数组：");
+            foreach (int n in list)
+                Console.Write(n + " ");
+            Console.WriteLine();
+            list.Clear();//移除所有元素
+            Console.Write("删除后的数组：");
+            foreach (int n in list)
+                Console.Write(n + " ");
+            Console.WriteLine();
+        }
+        static void RemoveArrayList()
+        {
+            ArrayList list = new ArrayList();
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(i);
+            }
+            Console.Write("原数组：");
+            foreach (int n in list)
+                Console.Write(n + " ");
+            Console.WriteLine();
+            list.Remove(3);//移除与“3”匹配的元素
+            Console.Write("删除后的数组：");
+            foreach (int n in list)
+                Console.Write(n + " ");
+            Console.WriteLine();
+        }
+        static void RemoveAtArrayList()
+        {
+            ArrayList list = new ArrayList();
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(i);
+            }
+            Console.Write("原数组：");
+            foreach (int n in list)
+                Console.Write(n + " ");
+            Console.WriteLine();
+            list.RemoveAt(3);//移除索引为3的元素
+            Console.Write("删除后的数组：");
+            foreach (int n in list)
+                Console.Write(n + " ");
+            Console.WriteLine();
+        }
+        static void RemoveRangeArrayList()
+        {
+            ArrayList list = new ArrayList();
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(i);
+            }
+            Console.Write("原数组：");
+            foreach (int n in list)
+                Console.Write(n + " ");
+            Console.WriteLine();
+            list.RemoveRange(3,4);//移除索引为3处4个元素
+            Console.Write("删除后的数组：");
+            foreach (int n in list)
+                Console.Write(n + " ");
+            Console.WriteLine();
+        }
+    }
+}

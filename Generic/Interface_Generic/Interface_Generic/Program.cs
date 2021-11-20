1eb90fa9a8bed023e5e46414c12555165f7e21b8
CalacalaBoom <zhangyu19990502@163.com>
2021-11-20 15:55:51 +0800

    泛型接口

---
+++ Generic/Interface_Generic/Interface_Generic/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_Generic
+{
+    //创建一个泛型接口
+    public interface IGenericInterface<T>
+    {
+        T CreateInterface();
+    }
+    //实现上面泛型接口的泛型类
+    public class Factory<T, T1> : IGenericInterface<T1> where T : T1, new()
+    {
+        public T1 CreateInterface()
+        {
+            return new T();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IGenericInterface<System.ComponentModel.IListSource> factory = 
+                new Factory<System.Data.DataTable, System.ComponentModel.IListSource>();
+            //输出指定泛型的类型
+            Console.WriteLine(factory.CreateInterface().GetType().ToString());
+            Console.ReadLine();
+        }
+    }
+}
